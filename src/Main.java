bcd8b4e14cecae7fcc0b43a9188158e4ced7bd43
Yulian Akshov <AKsh4751@gmail.com>
2023-05-04 22:37:12 +0300

    add gitignore, remove out, fix code

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int initialBalance = 100;
-        int refill = 2345;
+        int initialBalance = 350;
+        int refill = 1001;
         int bonus = refill / 100;
         int finalBalance;
-        if (refill >= 1000) {
+        if (refill > 1000) {
             finalBalance = (initialBalance + refill + bonus);
             System.out.println("Ваш баланс:" + finalBalance + ". Бонусных рублей :" + bonus );
-        }
-        if (refill < 1000) {
+        } else {
             finalBalance = (initialBalance + refill);
             System.out.println("Ваш баланс:" + finalBalance + ". Бонусных рублей :0" );
         }
